a2e59194d4189d753e5df3274a9a8adc37cff1bc
Daniel Heyns <daniel.heyns@ucalgary.ca>
2018-04-06 11:39:14 -0600

    FileHelper progress

--- FinalProject/src/FileHelper.java
+++ FinalProject/src/FileHelper.java
@@ -1,3 +1,8 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -5,15 +10,65 @@ import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 
 public class FileHelper {
-	public String fileChooser(JFrame frame) {
+	private String courseID;
+	private String AssignID;
+	
+	public FileHelper(String ID, String Assign) {
+		this.courseID = ID;
+		this.AssignID = Assign;
+	}
+	
+	public static void main(String args[]) throws IOException {
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		FileHelper filehelper = new FileHelper("Easy", "Creazy");
+		filehelper.fileChooserFile(frame);
+	}
+
+	public String fileChooserFile(JFrame frame) throws IOException {
 		JFileChooser chooser = new JFileChooser();
-		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		chooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
 		int returnVal = chooser.showOpenDialog(frame);
-		if(returnVal == JFileChooser.APPROVE_OPTION) {
-		   
-		        return chooser.getSelectedFile().getAbsolutePath();
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			File file = chooser.getSelectedFile();
+			String ext = getExtension(chooser.getSelectedFile().getAbsolutePath());
+			File copy = new File("C:\\Assigns\\"+ courseID + "\\" + AssignID + "." +ext);
+			copyFile(file,copy);
+			copy.getParentFile().mkdir();
+			copy.createNewFile();
+			return chooser.getSelectedFile().getAbsolutePath();
 		}
 		else
 			return null;
 	}
+
+	public static void copyFile(File sourceFile, File destFile) throws IOException {
+		if (!destFile.exists()) {
+			destFile.createNewFile();
+		}
+
+		FileChannel source = null;
+		FileChannel destination = null;
+
+		try {
+			source = new FileInputStream(sourceFile).getChannel();
+			destination = new FileOutputStream(destFile).getChannel();
+			destination.transferFrom(source, 0, source.size());
+		} finally {
+			if (source != null) {
+				source.close();
+			}
+			if (destination != null) {
+				destination.close();
+			}
+		}
+	}
+	
+	public static String getExtension(String path) {
+		
+		String ext[] = path.split("\\.");
+		return ext[1];
+	}
+
 }
